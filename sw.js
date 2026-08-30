3e1692e371b34d9e43048d7df100dfe205b91f2f
Maskywg <maskywg@gmail.com>
2026-08-30 18:59:02 +0800

    feat: 王胤安週課表 PWA App
    台大化學系大一課表，含空堂/衝堂檢查工具，可加入手機主畫面使用

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "wang-yinan-schedule-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
